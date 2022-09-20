a22ec255c464964614e9ff0446f83102daaa94bf
Johnson Hsu <specialline4107@gmail.com>
2022-09-20 23:26:45 +0800

    Update average message length

--- read.py
+++ read.py
@@ -2,12 +2,18 @@ data = []
 count = 0 # loop count
 with open('reviews.txt', 'r') as f:
 	for line in f:
-		data.append(line.strip())
+		data.append(line)
 		count += 1
 		if count % 1000 == 0:
-			print('We are running to', count, 'th message')
+			print('We are running to', count, 'th message') # print every 1000 loop
 
 print('In total, we have', len(data), 'message')
+
+# Calculate average message length
+sum_length = 0 # initial length
+for d in data:
+	sum_length += len(d)
+print('Average message length is:', sum_length / len(data))
 
 # Show ?th message as user's requirement
 while True:
